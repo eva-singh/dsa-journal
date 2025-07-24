b68ba4790940766fbfd4a273c939d0f2fe2fce39
Eva Singh <esingh1_be23@thapar.edu>
2025-07-24 23:41:56 +0530

    Practice

---
+++ 58-Length-of-Last-Word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.length();
+        int count=0;
+        int i=n-1;
+            while(i>=0 && s[i]==' ') i--;
+            while(i>=0 && s[i]!=' '){
+                count=count+1;
+                i--;
+            }         
+        return count;
+    }
+};
